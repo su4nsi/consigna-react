278fc6443eb80fa738b8aad371160c0cf6e1f518
miguel.pera <miguel.pera@estudiantat.upc.edu>
2025-05-14 12:37:27 +0200

    style: add pagination style

--- miguel pokeapi/src/components/pagination/Pagination.jsx
+++ miguel pokeapi/src/components/pagination/Pagination.jsx
@@ -20,14 +20,40 @@ const Pagination = ({ index, setIndex, total }) => {
         onClick={() => setIndex(handleFirstPage())}
         disabled={index === 0}
       >
-        First
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="pagination-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+          />
+        </svg>
       </button>
       <button
         className="pagination-button"
         onClick={() => setIndex(handlePreviousPage(index))}
         disabled={index === 0}
       >
-        Previous
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="pagination-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
       </button>
       {visibleIndexes.map((pageIndex) => (
         <button
@@ -44,14 +70,40 @@ const Pagination = ({ index, setIndex, total }) => {
         onClick={() => setIndex(handleNextPage(index))}
         disabled={index === total - 1}
       >
-        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="pagination-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </button>
       <button
         className="pagination-button"
         onClick={() => setIndex(handleLastPage())}
         disabled={index === total - 1}
       >
-        Last
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="pagination-icon"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </button>
     </div>
   );
