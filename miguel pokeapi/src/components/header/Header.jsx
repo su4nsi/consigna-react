13f6e6fe87e28be453923f573ca151da695d9da3
su4nsi <miguelperatorres@gmail.com>
2025-06-06 13:01:02 +0200

    fix: changed import

--- miguel pokeapi/src/components/header/Header.jsx
+++ miguel pokeapi/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import NavigationDesktop from "../navigation/desktop/NavigationDesktop";
 import NavigationResponsive from "../navigation/mobile/NavigationMobile";
-import { useHeaderLogic } from "./UseHeaderLogic";
+import { useHeaderLogic } from "./UseHeaderLogic.jsx";
 
 export function Header() {
   const { isMobile } = useHeaderLogic();
