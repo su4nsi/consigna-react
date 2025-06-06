663ab1740c92579fe07a8eb37ddb097bcfe1dc70
su4nsi <miguelperatorres@gmail.com>
2025-06-06 12:01:12 +0200

    chore. render app deploy

--- miguel pokeapi/src/main.jsx
+++ miguel pokeapi/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-createRoot(document.getElementById('root')).render(
+app.get("/", (req, res) => res.type("html").send(html));
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
