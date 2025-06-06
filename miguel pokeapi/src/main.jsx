dd4f6eeaf641a6acff16db6294c75e453d53ca13
su4nsi <miguelperatorres@gmail.com>
2025-06-06 12:56:18 +0200

    fix: fixes to run vercel deploy

--- miguel pokeapi/src/main.jsx
+++ miguel pokeapi/src/main.jsx
@@ -2,18 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.type("html").send(html));
-
-const server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
