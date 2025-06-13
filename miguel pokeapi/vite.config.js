02eaf80eec293712d4d89fb42eb886f69d8c258d
su4nsi <miguelperatorres@gmail.com>
2025-06-13 11:49:18 +0200

    fix: vercel deploy fix

--- miguel pokeapi/vite.config.js
+++ miguel pokeapi/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/pokemigapi/",
+  base: "/",
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.js",
-    include: ["**/*.test.js", "**/*.test.jsx"], 
+    include: ["**/*.test.js", "**/*.test.jsx"],
   },
 });
